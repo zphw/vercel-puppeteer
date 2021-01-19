c792461e147033d2c00303a263e22d678e2a6004
zphw <zph@zph.io>
2021-01-19 20:23:29 +0800

    show error

--- screenshot.js
+++ screenshot.js
@@ -45,6 +45,6 @@ module.exports = async function (req, res) {
     } catch (e) {
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
+        res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>' + e);
     }
 };
