3fdbf04f2b645d0903bd581dc433eb4d71268b34
zphw <zph@zph.io>
2021-01-19 23:35:06 +0800

    prod

--- chromium.js
+++ chromium.js
@@ -2,37 +2,34 @@ const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 async function createBrowser() {
-    const browser = await puppeteer.launch({
-        args: chrome.args,
+    const options = {
+        args: [...chrome.args, '--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', '--window-position=0,0', '--ignore-certifcate-errors', '--ignore-certifcate-errors-spki-list'],
         executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    });
+        headless: chrome.headless,
+        ignoreHTTPSErrors: true,
+    };
+
+    const browser = await puppeteer.launch(options);
     return browser;
 }
 
 async function getContent(url) {
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    });
+    const browser = await createBrowser();
 
     const page = await browser.newPage();
-    await page.goto(url);
+    const response = await page.goto(url);
+    await page.setContent((await response.buffer()).toString('utf8'));
     const content = await page.content();
     browser.close();
     return content;
 }
 
 async function getScreenshot(url, type, quality) {
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    });
+    const browser = await createBrowser();
 
     const page = await browser.newPage();
-    await page.goto(url);
+    const response = await page.goto(url);
+    await page.setContent((await response.buffer()).toString('utf8'));
     const file = await page.screenshot({ type, quality, fullPage: true });
     browser.close();
     return file;
